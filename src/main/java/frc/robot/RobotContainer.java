1093dcf1ccf00162ccce9b57f1b0e876ee392f1a
Tim Shmidt <timsh007@gmail.com>
2025-01-06 21:20:20 -0800

    A bunch of auton work with paths and autos on path planner, Also made a new path generator class to hopefully eventually make paths on the fly that avoid obstacles

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,6 +11,7 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 
+import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
