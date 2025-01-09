a9d1c1de510c2cf3341e8493f69419a2ff52f605
viannamosh <vianna.mosh@gmail.com>
2025-01-08 19:09:56 -0800

    mod auto

--- src/main/java/frc/robot/commands/AutoCommands/ModularAuto.java
+++ src/main/java/frc/robot/commands/AutoCommands/ModularAuto.java
@@ -1,8 +1,9 @@
 package frc.robot.commands.AutoCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-public class ModularAuto extends Command {
+public class ModularAuto extends SequentialCommandGroup {
 
     // TODO: Create the modular auto command/system, which takes the selected Auto destinations and runs robot between them using PathPlanner's Pathfinding
 
@@ -29,4 +30,6 @@ public class ModularAuto extends Command {
     public boolean isFinished() {
         return false;
     }
+
+
 }
