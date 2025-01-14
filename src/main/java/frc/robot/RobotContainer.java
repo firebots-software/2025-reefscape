5447966a4ef7621a163f361e03e4742024c55b57
73anthonyL <73anthony.lu@gmail.com>
2025-01-13 19:35:51 -0800

    zero button

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,7 +17,9 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.Nat;
 import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -92,6 +94,11 @@ public class RobotContainer {
          * Joystick B = dynamic forward
          * Joystick X = dyanmic reverse
          */
+
+        joystick.x().onTrue(
+            driveTrain.runOnce(() ->
+                driveTrain.resetPose(
+                    new Pose2d(new Translation2d(0.48, 4),Rotation2d.fromDegrees(0)))));
     }
 
     public static void setAlliance() {
