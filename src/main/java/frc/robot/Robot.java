e8e3ab34990f9b60a8fc77a82fa5723500715c45
nutella399 <nutella399@gmail.com>
2025-01-19 00:45:21 -0800

    linter moment

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -62,8 +62,7 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void disabledPeriodic() {
-  }
+  public void disabledPeriodic() {}
 
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
@@ -79,10 +78,7 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {
-    
-
-  }
+  public void autonomousPeriodic() {}
 
   @Override
   public void teleopInit() {
