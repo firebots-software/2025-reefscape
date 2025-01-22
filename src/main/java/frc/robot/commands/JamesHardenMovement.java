0010fe8b9a1a25be8019476a54ec2e373ebcc55d
73anthonyL <73anthony.lu@gmail.com>
2025-01-22 11:23:49 -0800

    removed smartdashboard import statement

--- src/main/java/frc/robot/commands/JamesHardenMovement.java
+++ src/main/java/frc/robot/commands/JamesHardenMovement.java
@@ -5,7 +5,6 @@ import static edu.wpi.first.units.Units.Meters;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.subsystems.SwerveSubsystem;
