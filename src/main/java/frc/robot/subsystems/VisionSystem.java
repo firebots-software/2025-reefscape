8b7adbd8c51f8911ad4ec04ca46429fdcb143efe
Ace-Kat <92191466+Ace-Kat@users.noreply.github.com>
2025-01-21 18:55:50 -0800

    updated photonvision

--- src/main/java/frc/robot/subsystems/VisionSystem.java
+++ src/main/java/frc/robot/subsystems/VisionSystem.java
@@ -8,10 +8,12 @@ import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Telemetry;
 import java.util.Optional;
@@ -23,15 +25,19 @@ import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+import com.ctre.phoenix6.SignalLogger;
+
+import dev.doglog.DogLog;
+
 /** Creates a new VisionSystem. */
 public class VisionSystem extends SubsystemBase {
   private final Telemetry logger = new Telemetry(10);
-  Pose3d savedResult = new Pose3d(0, 0, 0, new Rotation3d(0, 0, 0));
+  Pose2d savedResult = new Pose2d(0, 0, new Rotation2d(0.01, 0.01));
   private static PhotonCamera frontCamera = new PhotonCamera("front-camera");
   private PhotonCamera camera;
   private PhotonPipelineResult pipeline;
   // load 2025 april tag field layout
-  AprilTagFieldLayout aprilTagFieldLayout = AprilTagFields.k2025Reefscape.loadAprilTagLayoutField();
+  AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
   PhotonPoseEstimator photonPoseEstimator;
   // robot relative to camera **FIX FOR NEW ROBOT**
   Transform3d camToRobot =
@@ -85,21 +91,21 @@ public class VisionSystem extends SubsystemBase {
   }
 
   // without multitag, prolly not going to use this
-  public Pose3d getRobotPose3d() {
-    if (!pipeline.hasTargets()) {
-      return savedResult;
-    }
-    Optional<Pose3d> tagPose =
-        aprilTagFieldLayout.getTagPose(pipeline.getBestTarget().getFiducialId());
-    if (tagPose.isEmpty()) {
-      return savedResult;
-    } else {
-      savedResult =
-          PhotonUtils.estimateFieldToRobotAprilTag(
-              getTransformToTarget(), tagPose.get(), camToRobot);
-    }
-    return savedResult;
-  }
+  // public Pose3d getRobotPose3d() {
+  //   if (!pipeline.hasTargets()) {
+  //     return savedResult;
+  //   }
+  //   Optional<Pose3d> tagPose =
+  //       aprilTagFieldLayout.getTagPose(pipeline.getBestTarget().getFiducialId());
+  //   if (tagPose.isEmpty()) {
+  //     return savedResult;
+  //   } else {
+  //     savedResult =
+  //         PhotonUtils.estimateFieldToRobotAprilTag(
+  //             getTransformToTarget(), tagPose.get(), camToRobot);
+  //   }
+  //   return savedResult;
+  // }
 
   @Override
   public void periodic() {
@@ -109,6 +115,12 @@ public class VisionSystem extends SubsystemBase {
     // } else {
     //   SmartDashboard.putBoolean("No Pose", false);
     // }
-    logger.logVisionPose(frontSystem.getMultiTagPose3d(new Pose2d()));
+    Optional<EstimatedRobotPose> pose = frontSystem.getMultiTagPose3d(savedResult);
+    logger.logVisionPose(pose);
+    if(!pose.isEmpty()){
+      SmartDashboard.putNumber("VisionX", pose.get().estimatedPose.getX());
+      SmartDashboard.putNumber("VisionY", pose.get().estimatedPose.getY());
+      savedResult = pose.get().estimatedPose.toPose2d();
+    }
   }
 }
