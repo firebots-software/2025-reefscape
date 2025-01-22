dce212d3c261bd78302fe1d482d2cdc11c353fc8
Ace-Kat <92191466+Ace-Kat@users.noreply.github.com>
2025-01-21 20:47:33 -0800

    Co-authored-by: Maryam <MaryamShaikh320@users.noreply.github.com> added shootersim

--- src/main/java/frc/robot/subsystems/TootsieSlideSubsystem.java
+++ src/main/java/frc/robot/subsystems/TootsieSlideSubsystem.java
@@ -7,9 +7,23 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.RobotController;
+// Simulation imports
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+import edu.wpi.first.wpilibj.simulation.PWMSim;
+import edu.wpi.first.wpilibj.simulation.BatterySim;
+import edu.wpi.first.wpilibj.simulation.RoboRioSim;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N2;
+import edu.wpi.first.math.system.LinearSystem;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.util.LoggedTalonFX;
 import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
@@ -23,7 +37,8 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 import dev.doglog.DogLog;
 import frc.robot.Constants;
 
-public class TootsieSlideSubsystem extends SubsystemBase {
+
+public class TootsieSlideSubsystem extends SubsystemBase{
     private static TootsieSlideSubsystem instance;
     
     private DigitalInput coralSensor;
@@ -31,7 +46,16 @@ public class TootsieSlideSubsystem extends SubsystemBase {
     private LoggedTalonFX downMotor;
     private LoggedTalonFX master;
 
+    private final DCMotor m_tootsieSlideGearbox = DCMotor.getKrakenX60(1);
+
+    public static LinearSystem<N1,N1,N1> tootsieSystem = LinearSystemId.identifyVelocitySystem(1.0 , 1.0);
+
+    private final FlywheelSim m_flywheelSim = new FlywheelSim(tootsieSystem, m_tootsieSlideGearbox, 1.0);
+
+
   public TootsieSlideSubsystem() {
+
+    
     upMotor = new LoggedTalonFX(1); // Unique ID for motor1
     downMotor = new LoggedTalonFX(2); // Unique ID for motor2
 
@@ -77,12 +101,15 @@ public static TootsieSlideSubsystem getInstance() {
 
   public void setPosition(double pos) {
     master.setControl(new MotionMagicVoltage(pos));
-  }
+    m_flywheelSim.setInputVoltage(master.getMotorVoltage().getValue().magnitude());
+    }
 
   private void runTootsieAtRPS(double speed) {
     VelocityVoltage m_velocityControl =
         new VelocityVoltage(speed * Constants.TootsieSlide.GEAR_RATIO);
     master.setControl(m_velocityControl);
+
+    m_flywheelSim.setInputVoltage(master.getMotorVoltage().getValue().magnitude());
   }
 
   public void spinTootsie() {
@@ -92,6 +119,9 @@ public static TootsieSlideSubsystem getInstance() {
    public void stopTootsie() {
     master.stopMotor();
   }
+
+    // Simulation
+
 
 
   /**
@@ -111,7 +141,19 @@ public static TootsieSlideSubsystem getInstance() {
 
   @Override
   public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
+    // In this method, we update our simulation of what our elevator is doing
+    // First, we set our "inputs" (voltages)
+    // m_flywheelSim.setInputVolatge()
+
+    // // Next, we update it. The standard loop time is 20ms.
+    // m_flywheelSim.update(0.020);
+
+    // // Finally, we set our simulated encoder's readings and simulated battery voltage
+    // m_flywheelSim.setDistance(m_flywheelSim.getPositionMeters());
+    // // SimBattery estimates loaded battery voltages
+    // RoboRioSim.setVInVoltage(
+    //     BatterySim.calculateDefaultBatteryLoadedVoltage(m_flywheelSim.getCurrentDrawAmps()));
+    SmartDashboard.putNumber("Flywheel Velocity", m_flywheelSim.getAngularVelocityRadPerSec());
   }
 
 public boolean coralPresent() {
