d703ef032227753e6e793e115d5be58a2712473d
Tim Shmidt <timsh007@gmail.com>
2025-01-28 21:16:56 -0800

    split top and bottom paths into constituant parts, sort of figured out how to run command sequence in getAutonomousCommand

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ package frc.robot;
 import java.util.function.DoubleSupplier;
 
 import com.ctre.phoenix6.SignalLogger;
+import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 
 import choreo.auto.AutoFactory;
@@ -33,124 +34,109 @@ public class RobotContainer {
   private static Matrix<N3, N1> visionMatrix = VecBuilder.fill(0.01, 0.03d, 100d);
   private static Matrix<N3, N1> odometryMatrix = VecBuilder.fill(0.1, 0.1, 0.1);
 
-  //private final AutoFactory autoFactory;
-  // Alliance color
-  //private BooleanSupplier redside = () -> redAlliance;
-  private static boolean redAlliance;
-
-  private final SwerveSubsystem driveTrain =
-      new SwerveSubsystem(
-          Constants.Swerve.DrivetrainConstants,
-          Constants.Swerve.Odometry.ODOMETRY_UPDATE_CONSTANT,
-          odometryMatrix,
-          visionMatrix,
-          Constants.Swerve.FrontLeft,
-          Constants.Swerve.FrontRight,
-          Constants.Swerve.BackLeft,
-          Constants.Swerve.BackRight);
-
-  private final Telemetry logger =
-      new Telemetry(Constants.Swerve.PHYSICAL_MAX_SPEED_METERS_PER_SECOND);
-  private final CommandXboxController joystick = new CommandXboxController(0);
-
-  // Starts telemetry operations (essentially logging -> look on SmartDashboard,
-  // AdvantageScope)
-  public void doTelemetry() {
-    logger.telemeterize(driveTrain.getCurrentState());
-  }
-
-  // public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
-
-  /* Modular Auto Selection */
-  private final SendableChooser<String> startPosChooser = new SendableChooser<String>();
-
-  /**
- * 
- */
-public RobotContainer() {
-    // autoChooser = AutoBuilder.buildAutoChooser("Tests");
-    // SmartDashboard.putData("Auto Mode", autoChooser);
-
-    // TODO: Create 6 SendableChooser objects: Alliance, Start Position, Reef 1-3,
-    // End Position.
-    // TODO: Figure out which options to add to the choosers, and how to convert
-    // those string
-    // choices to the Pose2d's in Constants.
-    // TODO: Use commands > AutoCommands > ModularAuto.java command to run the auto
-    // using the
-    // selected options and Pose2d list
-    SmartDashboard.putString("follow_traj_running", "no");
-    
-    /*
-    autoFactory =
+  private AutoFactory autoFactory;
+    // Alliance color
+    //private BooleanSupplier redside = () -> redAlliance;
+    private static boolean redAlliance;
+  
+    private final SwerveSubsystem driveTrain =
+        new SwerveSubsystem(
+            Constants.Swerve.DrivetrainConstants,
+            Constants.Swerve.Odometry.ODOMETRY_UPDATE_CONSTANT,
+            odometryMatrix,
+            visionMatrix,
+            Constants.Swerve.FrontLeft,
+            Constants.Swerve.FrontRight,
+            Constants.Swerve.BackLeft,
+            Constants.Swerve.BackRight);
+  
+    private final Telemetry logger =
+        new Telemetry(Constants.Swerve.PHYSICAL_MAX_SPEED_METERS_PER_SECOND);
+    private final CommandXboxController joystick = new CommandXboxController(0);
+  
+    // Starts telemetry operations (essentially logging -> look on SmartDashboard,
+    // AdvantageScope)
+    public void doTelemetry() {
+      logger.telemeterize(driveTrain.getCurrentState());
+    }
+  
+    // public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+  
+    /* Modular Auto Selection */
+    private final SendableChooser<String> startPosChooser = new SendableChooser<String>();
+  
+    /**
+   * 
+   */
+  public RobotContainer() {
+      SmartDashboard.putString("follow_traj_running", "no");
+  
+      startPosChooser.setDefaultOption("Top (Next to Blue Barge Zone)", "top");
+      startPosChooser.addOption("Middle (In between to Barge Zones)", "Middle");
+      startPosChooser.addOption("Bottom (Next to Red Barge Zone)", "bottom");
+      SmartDashboard.putData("Auto Start Position", startPosChooser);
+  
+      configureBindings();
+      setAlliance();
+    }
+  
+    private void configureBindings() {
+      // Joystick suppliers,
+      Trigger leftShoulderTrigger = joystick.leftBumper();
+      DoubleSupplier
+          frontBackFunction = () -> ((redAlliance) ? joystick.getLeftY() : -joystick.getLeftY()),
+          leftRightFunction = () -> ((redAlliance) ? joystick.getLeftX() : -joystick.getLeftX()),
+          rotationFunction = () -> -joystick.getRightX(),
+          speedFunction =
+              () ->
+                  leftShoulderTrigger.getAsBoolean()
+                      ? 0d
+                      : 1d; // slowmode when left shoulder is pressed, otherwise fast
+      SwerveJoystickCommand swerveJoystickCommand =
+          new SwerveJoystickCommand(
+              frontBackFunction,
+              leftRightFunction,
+              rotationFunction,
+              speedFunction, // slowmode when left shoulder is pressed, otherwise fast
+              () -> joystick.leftTrigger().getAsBoolean(),
+              driveTrain);
+      driveTrain.setDefaultCommand(swerveJoystickCommand);
+  
+      joystick.povUp().onTrue(Commands.runOnce(SignalLogger::start));
+      joystick.povDown().onTrue(Commands.runOnce(SignalLogger::stop));
+  
+      /*
+       * Joystick Y = quasistatic forward
+       * Joystick A = quasistatic reverse
+       * Joystick B = dynamic forward
+       * Joystick X = dyanmic reverse
+       */
+  
+      joystick.y().whileTrue(driveTrain.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+      joystick.a().whileTrue(driveTrain.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+      joystick.b().whileTrue(driveTrain.sysIdDynamic(SysIdRoutine.Direction.kForward));
+      joystick.x().whileTrue(driveTrain.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+    }
+  
+    public void setAlliance() {
+      redAlliance =
+          (DriverStation.getAlliance().isEmpty())
+              ? false
+              : (DriverStation.getAlliance().get() == Alliance.Red);
+    }
+  
+    public Command getAutonomousCommand() {
+      /* Run the path selected from the auto chooser */
+      //return new PathPlannerAuto("top", redAlliance ^ startPosChooser.getSelected() == "bottom"); // flips when red or bottom, doesn't flip if both
+      boolean autoAlliance = redAlliance;
+      autoFactory =
         new AutoFactory(
             () -> driveTrain.getState().Pose,
             driveTrain::resetPose,
             driveTrain::followTrajectory,
             true,
             driveTrain);
-    */
-        
-            
-
-    startPosChooser.setDefaultOption("Top (Next to Blue Barge Zone)", "top");
-    //startPosChooser.addOption("Middle (In between to Barge Zones)", "Middle");
-    startPosChooser.addOption("Bottom (Next to Red Barge Zone)", "bottom");
-    SmartDashboard.putData("Auto Start Position", startPosChooser);
-
-    configureBindings();
-    setAlliance();
-  }
-
-  private void configureBindings() {
-    // Joystick suppliers,
-    Trigger leftShoulderTrigger = joystick.leftBumper();
-    DoubleSupplier
-        frontBackFunction = () -> ((redAlliance) ? joystick.getLeftY() : -joystick.getLeftY()),
-        leftRightFunction = () -> ((redAlliance) ? joystick.getLeftX() : -joystick.getLeftX()),
-        rotationFunction = () -> -joystick.getRightX(),
-        speedFunction =
-            () ->
-                leftShoulderTrigger.getAsBoolean()
-                    ? 0d
-                    : 1d; // slowmode when left shoulder is pressed, otherwise fast
-    SwerveJoystickCommand swerveJoystickCommand =
-        new SwerveJoystickCommand(
-            frontBackFunction,
-            leftRightFunction,
-            rotationFunction,
-            speedFunction, // slowmode when left shoulder is pressed, otherwise fast
-            () -> joystick.leftTrigger().getAsBoolean(),
-            driveTrain);
-    driveTrain.setDefaultCommand(swerveJoystickCommand);
-
-    joystick.povUp().onTrue(Commands.runOnce(SignalLogger::start));
-    joystick.povDown().onTrue(Commands.runOnce(SignalLogger::stop));
-
-    /*
-     * Joystick Y = quasistatic forward
-     * Joystick A = quasistatic reverse
-     * Joystick B = dynamic forward
-     * Joystick X = dyanmic reverse
-     */
-
-    joystick.y().whileTrue(driveTrain.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-    joystick.a().whileTrue(driveTrain.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-    joystick.b().whileTrue(driveTrain.sysIdDynamic(SysIdRoutine.Direction.kForward));
-    joystick.x().whileTrue(driveTrain.sysIdDynamic(SysIdRoutine.Direction.kReverse));
-  }
-
-  public void setAlliance() {
-    redAlliance =
-        (DriverStation.getAlliance().isEmpty())
-            ? false
-            : (DriverStation.getAlliance().get() == Alliance.Red);
-  }
-
-  public Command getAutonomousCommand() {
-    /* Run the path selected from the auto chooser */
-    return new PathPlannerAuto(startPosChooser.getSelected(), redAlliance); // flips when red
-
-    //return autoFactory.trajectoryCmd(startPosChooser.getSelected());
+    Command auto = Commands.sequence(autoFactory.resetOdometry(startPosChooser.getSelected() == "middle" ? "middle " : "" + "start to reef1"));
+    return auto;
   }
 }
