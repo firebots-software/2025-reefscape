f1b6b82c3362681e0a1286057c069567c58bd2f2
73anthonyL <73anthony.lu@gmail.com>
2025-01-29 18:51:12 -0800

    choreo fixes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,17 +15,27 @@ import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.JamesHardenMovement;
 import frc.robot.commands.SwerveJoystickCommand;
 import frc.robot.subsystems.SwerveSubsystem;
+
+import java.util.function.BiConsumer;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
+import com.pathplanner.lib.path.PathPlannerPath;
+
+import choreo.auto.AutoChooser;
 import choreo.auto.AutoFactory;
+import choreo.auto.AutoRoutine;
+import dev.doglog.DogLog;
 
 public class RobotContainer {
   private static Matrix<N3, N1> visionMatrix = VecBuilder.fill(0.01, 0.03d, 100d);
@@ -46,11 +56,12 @@ public class RobotContainer {
           Constants.Swerve.BackLeft,
           Constants.Swerve.BackRight);
 
-  private final AutoFactory autoFactory;
-  
+
   private final Telemetry logger =
       new Telemetry(Constants.Swerve.PHYSICAL_MAX_SPEED_METERS_PER_SECOND);
   private final CommandXboxController joystick = new CommandXboxController(0);
+
+  private final AutoFactory autoFactory;
 
   // Starts telemetry operations (essentially logging -> look on SmartDashboard, AdvantageScope)
   public void doTelemetry() {
@@ -67,7 +78,7 @@ public class RobotContainer {
             driveTrain 
         );
   }
-
+    
   private void configureBindings() {
     // Joystick suppliers,
     Trigger leftShoulderTrigger = joystick.leftBumper();
@@ -144,7 +155,8 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    /* Run the path selected from the auto chooser */
-    return autoFactory.trajectoryCmd("TestPath");
+    var routine = autoFactory.newRoutine("routine");
+        routine.active().onTrue(routine.trajectory("TestPath").resetOdometry().andThen(routine.trajectory("TestPath").cmd()));
+    return routine.cmd();
   }
 }
