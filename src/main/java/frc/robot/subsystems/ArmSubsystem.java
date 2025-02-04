733c3bc0541776a9704603da361e2b714befed97
Warzone <yajwin.grover@gmail.com>
2025-02-03 18:44:09 -0800

    Made velocity control for flywheel rather than position control

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -16,6 +16,7 @@ import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
@@ -130,7 +131,7 @@ public class ArmSubsystem extends SubsystemBase {
  
   public void startFlywheel() {
     flywheelMotor.setControl(
-        controlRequestFlywheel.withPosition(Constants.Flywheel.ANGLE_TO_ENCODER_ROTATIONS(angleDegrees)));
+        new VelocityVoltage(30));
   }
 
   public void stopEveryingONG(){
