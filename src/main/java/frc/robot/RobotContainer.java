4c220b52209dfda8b485e2562063f9132ea0b45e
Violet Su <violetnsu@gmail.com>
2025-02-05 20:16:03 -0800

    2/5 creating a button for another position

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -151,6 +151,8 @@ public class RobotContainer {
     joystick.povDown().onTrue(new SetElevatorLevel(m_ElevatorSubsystem, ElevatorPositions.L3));
     joystick.povLeft().onTrue(new SetElevatorLevel(m_ElevatorSubsystem, ElevatorPositions.L4));
 
+    joystick.a().whileTrue(new SetElevatorLevel(m_ElevatorSubsystem, ElevatorPositions.pos)); // change pos in constants 
+
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
   }
