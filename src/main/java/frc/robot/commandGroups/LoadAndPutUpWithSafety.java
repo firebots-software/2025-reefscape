bd34d256fe465d28d19c540d1484d4da2f9d336e
Warzone <yajwin.grover@gmail.com>
2025-02-05 19:58:33 -0800

    Added checks to make sure that the elevator is at location before starting funnel

--- src/main/java/frc/robot/commandGroups/LoadAndPutUpWithSafety.java
+++ src/main/java/frc/robot/commandGroups/LoadAndPutUpWithSafety.java
@@ -6,10 +6,10 @@ package frc.robot.commandGroups;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.ElevatorConstants.ElevatorPositions;
+import frc.robot.commands.TransferPieceBetweenFunnelAndElevator;
 import frc.robot.commands.ElevatorCommands.ElevatorIntakeLevel;
 import frc.robot.commands.ElevatorCommands.SetElevatorLevel;
 import frc.robot.commands.FunnelCommands.RunFunnelUntilDetection;
-import frc.robot.commands.TransferPieceBetweenFunnelAndElevator;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.FunnelSubsystem;
 import frc.robot.subsystems.TootsieSlideSubsystem;
@@ -17,17 +17,10 @@ import frc.robot.subsystems.TootsieSlideSubsystem;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class LoadAndPutUp extends SequentialCommandGroup {
-
-  public LoadAndPutUp(
-      ElevatorSubsystem elevator,
-      FunnelSubsystem funnel,
-      TootsieSlideSubsystem tootsieSlideSubsystem,
-      ElevatorPositions level) {
+public class LoadAndPutUpWithSafety extends SequentialCommandGroup {
+  /** Creates a new LoadAndPutUpWithSafety. */
+  public LoadAndPutUpWithSafety( ElevatorSubsystem elevator, FunnelSubsystem funnel, TootsieSlideSubsystem tootsieSlideSubsystem, ElevatorPositions level) {
     // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new ElevatorIntakeLevel(elevator, funnel).andThen(new RunFunnelUntilDetection(funnel)));
-    addCommands(new TransferPieceBetweenFunnelAndElevator(elevator, funnel, tootsieSlideSubsystem));
-    addCommands(new SetElevatorLevel(elevator, level));
+    // addCommands(new FooCommand(), new BarCommand())
   }
 }
