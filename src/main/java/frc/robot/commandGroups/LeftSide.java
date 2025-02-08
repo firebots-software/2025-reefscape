7e4f436cc6297d6b78059e8b3c4986166cab093c
Violet Su <violetnsu@gmail.com>
2025-02-08 10:40:55 -0800

    2/8 RITVIK PLS CEHCKKKK i did the right and left side

--- src/main/java/frc/robot/commandGroups/LeftSide.java
+++ src/main/java/frc/robot/commandGroups/LeftSide.java
@@ -5,21 +5,20 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.ElevatorConstants.ElevatorPositions;
 import frc.robot.commands.ElevatorCommands.SetElevatorLevel;
 import frc.robot.commands.SwerveCommands.JamesHardenMovement;
-import frc.robot.commands.SwerveCommands.SwerveJoystickCommand;
 import frc.robot.commands.TootsieSlideCommands.ShootTootsieSlide;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.subsystems.TootsieSlideSubsystem;
 
-public class ElevatorAndShooting extends SequentialCommandGroup {
-  public ElevatorAndShooting(
+public class LeftSide extends SequentialCommandGroup {
+  public LeftSide(
       ElevatorSubsystem elevatorSubsystem,
       TootsieSlideSubsystem tootsieSlideSubsystem,
       SwerveSubsystem swerveSubsystem,
       ElevatorPositions height,
-      Pose2d isLeft) { 
+      Pose2d leftPose) { 
     addCommands(
-        new SetElevatorLevel(elevatorSubsystem, height).alongWith(new JamesHardenMovement(swerveSubsystem, isLeft)),  
+        new SetElevatorLevel(elevatorSubsystem, height).alongWith(new JamesHardenMovement(swerveSubsystem, leftPose)),  
         new ShootTootsieSlide(tootsieSlideSubsystem));   
   }
 }
--- src/main/java/frc/robot/commandGroups/RightSide.java
+++ src/main/java/frc/robot/commandGroups/RightSide.java
@@ -1,25 +1,26 @@
 package frc.robot.commandGroups;
 
+import java.util.function.Supplier;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.ElevatorConstants.ElevatorPositions;
 import frc.robot.commands.ElevatorCommands.SetElevatorLevel;
 import frc.robot.commands.SwerveCommands.JamesHardenMovement;
-import frc.robot.commands.SwerveCommands.SwerveJoystickCommand;
 import frc.robot.commands.TootsieSlideCommands.ShootTootsieSlide;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.subsystems.TootsieSlideSubsystem;
 
-public class ElevatorAndShooting extends SequentialCommandGroup {
-  public ElevatorAndShooting(
+public class RightSide extends SequentialCommandGroup {
+  public RightSide(
       ElevatorSubsystem elevatorSubsystem,
       TootsieSlideSubsystem tootsieSlideSubsystem,
       SwerveSubsystem swerveSubsystem,
       ElevatorPositions height,
-      Pose2d isLeft) { 
+      Pose2d righPose2d, Supplier<Pose2d> rightPose2d) { 
     addCommands(
-        new SetElevatorLevel(elevatorSubsystem, height).alongWith(new JamesHardenMovement(swerveSubsystem, isLeft)),  
+        new SetElevatorLevel(elevatorSubsystem, height).alongWith(new JamesHardenMovement(swerveSubsystem, rightPose2d)),  
         new ShootTootsieSlide(tootsieSlideSubsystem));   
   }
 }
