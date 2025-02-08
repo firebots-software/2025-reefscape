a9dfab0ffd70e6f7ed0dcf231d9d1756d6c36e64
Tim Shmidt <timsh007@gmail.com>
2025-02-08 14:05:58 -0800

    made vels more visible in gyrostab for testing

--- src/main/java/frc/robot/commands/GyroStabilizer.java
+++ src/main/java/frc/robot/commands/GyroStabilizer.java
@@ -33,8 +33,8 @@ public class GyroStabilizer extends Command {
   @Override
   public void execute() {
     Transform2d currentTipVectorRP = getTipVectorRP(pigeon);
-    SwerveRequest drive = robotCentricDrive.withVelocityX(-currentTipVectorRP.getY())
-                                           .withVelocityY(currentTipVectorRP.getX());
+    SwerveRequest drive = robotCentricDrive.withVelocityX(-currentTipVectorRP.getY() * 100)
+                                           .withVelocityY(currentTipVectorRP.getX() * 100);
     swerveSubsystem.setControl(drive);
   }
 
