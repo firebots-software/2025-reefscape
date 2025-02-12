13130ad48a64ead2ede97abc99d6fe4109ff44d9
Ace-Kat <92191466+Ace-Kat@users.noreply.github.com>
2025-02-11 21:12:47 -0800

    started abstractedPID to tune pid, added all the variables and sample logic
    Co-authored-by: NikashM <NikashM@users.noreply.github.com>

---
+++ src/main/java/frc/robot/commands/AbstractedPID.java
@@ -0,0 +1,27 @@
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ArmSubsystem;
+
+public class IncreaseP extends Command {
+
+
+
+  public AbstractedPID(ArmSubsystem arm) {
+        PIDc
+  }
+
+  @Override
+  public void initialize() {}
+
+  @Override
+  public void execute() {
+  }
+
+  @Override
+  public void end(boolean interrupted) {}
+
+  @Override
+  public boolean isFinished() {
+    
+  }
+}
