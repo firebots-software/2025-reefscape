d0bab1126d749e58d5690e2baf80ac5e6c5c3b84
Ace-Kat <92191466+Ace-Kat@users.noreply.github.com>
2025-02-15 11:11:27 -0800

    Fixed Merge conflict

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -138,21 +138,14 @@ public final class Constants {
   }
 
   public static class Flywheel {
-<<<<<<< HEAD
-    public static final int FLYWHEEL_PORT = 0;
-    public static final double MOTIONMAGIC_KV = 0.0;
-    public static final double MOTIONMAGIC_KA = 0.0;
-    public static double FLYWHEEL_S0C_KP = 0.0;
     public static double FLYWHEEL_S0C_KI = 0.0;
     public static double FLYWHEEL_S0C_KD = 0.0;
     public static double FLYWHEEL_S0C_KS = 0.0;
     public static double FLYWHEEL_S0C_KG = 0.0;
-=======
     public static final int FLYWHEEL_PORT = 17;
     public static final double MOTIONMAGIC_KV = 0;
     public static final double MOTIONMAGIC_KA = 0;
     public static final double FLYWHEEL_S0C_KP = 1.0;
->>>>>>> b1e2038453c06a0ee5de19358e4cf86602ba3123
     public static final double FLYWHEEL_SUPPLY_CURRENT_LIMIT_AMPS = 5.0;
     public static final double FLYWHEEL_STATOR_CURRENT_LIMIT_AMPS = 5.0;
 
