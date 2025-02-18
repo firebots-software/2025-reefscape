2d11c5bbd745d01d4ef3d8fd60afea5221a3507e
Ace-Kat <92191466+Ace-Kat@users.noreply.github.com>
2025-02-17 17:44:11 -0800

    changed current limit to 15 for flywheel

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -176,8 +176,8 @@ public final class Constants {
     public static final double MOTIONMAGIC_KV = 0;
     public static final double MOTIONMAGIC_KA = 0;
 
-    public static final double FLYWHEEL_SUPPLY_CURRENT_LIMIT_AMPS = 5.0;
-    public static final double FLYWHEEL_STATOR_CURRENT_LIMIT_AMPS = 5.0;
+    public static final double FLYWHEEL_SUPPLY_CURRENT_LIMIT_AMPS = 15.0;
+    public static final double FLYWHEEL_STATOR_CURRENT_LIMIT_AMPS = 15.0;
     public static final double FLYWHEEL_SPEED_RPS = 30.0;
 
     public static double ANGLE_TO_ENCODER_ROTATIONS(double angle) {
