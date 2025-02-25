0d6056db47a9627c4b20133fddeed95635317eb6
73anthonyL <73anthony.lu@gmail.com>
2025-02-24 18:41:16 -0800

    removed wait in jameshardenscorecloseset

--- src/main/java/frc/robot/commandGroups/JamesHardenScoreClosest.java
+++ src/main/java/frc/robot/commandGroups/JamesHardenScoreClosest.java
@@ -2,7 +2,6 @@ package frc.robot.commandGroups;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.ElevatorConstants.ElevatorPositions;
 import frc.robot.commands.ElevatorCommands.SetElevatorLevel;
 import frc.robot.commands.SwerveCommands.JamesHardenMovement;
