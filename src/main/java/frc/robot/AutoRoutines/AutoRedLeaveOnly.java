09fb9a351c558672f6ff5dc18f20405a7183f865
73anthonyL <73anthony.lu@gmail.com>
2025-02-27 06:45:53 -0800

    leave only autos made

---
+++ src/main/java/frc/robot/AutoRoutines/AutoRedLeaveOnly.java
@@ -0,0 +1,33 @@
+package frc.robot.AutoRoutines;
+
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.Constants.Landmarks;
+import frc.robot.Constants.ElevatorConstants.ElevatorPositions;
+import frc.robot.commandGroups.Intake;
+import frc.robot.commandGroups.JamesHardenScoreClosest;
+import frc.robot.commandGroups.PutUpAndShoot;
+import frc.robot.commands.ElevatorCommands.SetElevatorLevel;
+import frc.robot.commands.SwerveCommands.JamesHardenMovement;
+import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.subsystems.FunnelSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.subsystems.TootsieSlideSubsystem;
+
+public class AutoRedLeaveOnly extends SequentialCommandGroup {
+  private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+
+  public AutoRedLeaveOnly(
+      SwerveSubsystem driveTrain) {
+    addCommands(
+        new InstantCommand(
+            () -> driveTrain.resetPose(Constants.Landmarks.redMidAutoStart)), new JamesHardenMovement(driveTrain, new Pose2d(
+      new Translation2d(11.441716194152832, 4.050836891), new Rotation2d()), false));
+  }
+}
