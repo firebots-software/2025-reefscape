991003b097720e41e6717f58ee62eab54ab7ce73
73anthonyL <73anthony.lu@gmail.com>
2025-03-07 23:43:53 -0800

    harden cleaned up

--- src/main/java/frc/robot/commands/SwerveCommands/JamesHardenMovement.java
+++ src/main/java/frc/robot/commands/SwerveCommands/JamesHardenMovement.java
@@ -6,13 +6,55 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
+import frc.robot.Constants.BlueLandmarkPose;
+import frc.robot.Constants.LandmarkPose;
+import frc.robot.Constants.RedLandmarkPose;
 import frc.robot.subsystems.SwerveSubsystem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.function.BooleanSupplier;
 import java.util.function.Supplier;
 
 public class JamesHardenMovement extends Command {
 
   private final SwerveSubsystem swerve;
+
+  private static final ArrayList<RedLandmarkPose> redBranchesL = new ArrayList<>(Arrays.asList(
+    RedLandmarkPose.L0,
+    RedLandmarkPose.L1,
+    RedLandmarkPose.L2,
+    RedLandmarkPose.L3,
+    RedLandmarkPose.L4,
+    RedLandmarkPose.L5
+  ));
+
+  private static final ArrayList<RedLandmarkPose> redBranchesR = new ArrayList<>(Arrays.asList(
+    RedLandmarkPose.R0,
+    RedLandmarkPose.R1,
+    RedLandmarkPose.R2,
+    RedLandmarkPose.R3,
+    RedLandmarkPose.R4,
+    RedLandmarkPose.R5
+  ));
+
+  private static final ArrayList<BlueLandmarkPose> blueBranchesL = new ArrayList<>(Arrays.asList(
+    BlueLandmarkPose.L0,
+    BlueLandmarkPose.L1,
+    BlueLandmarkPose.L2,
+    BlueLandmarkPose.L3,
+    BlueLandmarkPose.L4,
+    BlueLandmarkPose.L5
+  ));
+
+  private static final ArrayList<BlueLandmarkPose> blueBranchesR = new ArrayList<>(Arrays.asList(
+    BlueLandmarkPose.R0,
+    BlueLandmarkPose.R1,
+    BlueLandmarkPose.R2,
+    BlueLandmarkPose.R3,
+    BlueLandmarkPose.R4,
+    BlueLandmarkPose.R5
+  ));
 
   private Supplier<Pose2d> targetPoseSupplier = null;
   private Pose2d targetPose = null;
@@ -66,9 +108,9 @@ public class JamesHardenMovement extends Command {
     double targetRot = targetPose.getRotation().getRadians();
     targetRot = ((2.0 * Math.PI) + (targetRot % (2.0 * Math.PI))) % (2.0 * Math.PI);
 
-    if ((Math.abs(swerve.getCurrentState().Pose.getX() - targetPose.getX()) < 0.02)
-        && (Math.abs(swerve.getCurrentState().Pose.getY() - targetPose.getY()) < 0.02)
-        && (Math.abs(targetRot - currRot) < 0.0075)) {
+    if ((Math.abs(swerve.getCurrentState().Pose.getX() - targetPose.getX()) < Constants.HardenConstants.RegularCommand.xyIndividualTolerance)
+        && (Math.abs(swerve.getCurrentState().Pose.getY() - targetPose.getY()) < Constants.HardenConstants.RegularCommand.xyIndividualTolerance)
+        && (Math.abs(targetRot - currRot) < Constants.HardenConstants.RegularCommand.headingTolerance)) {
       return true;
     } else return false;
   }
@@ -78,102 +120,75 @@ public class JamesHardenMovement extends Command {
     swerve.setRobotSpeeds(new ChassisSpeeds(0, 0, 0));
   }
 
-  public static JamesHardenMovement toSpecificLeftBranch(
-      SwerveSubsystem swerve, BooleanSupplier redSide, boolean isInAuto, int reefSideIndex) {
-    if (redSide.getAsBoolean()) {
-      return new JamesHardenMovement(
-          swerve,
-              Constants.Landmarks.LEFT_LINEUP_RED[reefSideIndex],
-          isInAuto);
-    } else {
-      return new JamesHardenMovement(
-          swerve,
-              Constants.Landmarks.LEFT_LINEUP_BLUE[reefSideIndex],
-          isInAuto);
-    }
-  }
-
   public Pose2d getTargetPose2d() {
     return targetPose;
   }
 
-  public static JamesHardenMovement toSpecificRightBranch(
-      SwerveSubsystem swerve, BooleanSupplier redSide, boolean isInAuto, int reefSideIndex) {
-    if (redSide.getAsBoolean()) {
-      return new JamesHardenMovement(
-          swerve,
-          Constants.Landmarks.RIGHT_LINEUP_RED[reefSideIndex],
-          isInAuto);
-    } else {
-      return new JamesHardenMovement(
-          swerve,
-              Constants.Landmarks.RIGHT_LINEUP_BLUE[reefSideIndex],
-          isInAuto);
-    }
+  public static JamesHardenMovement toSpecificBranch(SwerveSubsystem swerve, boolean isInAuto, LandmarkPose branch){
+    return new JamesHardenMovement(swerve, branch.getPose(), isInAuto);
   }
 
   public static JamesHardenMovement toClosestLeftBranch(
       SwerveSubsystem swerve, BooleanSupplier redSide, boolean isInAuto) {
 
-    Supplier<Integer> targetSide =
+    Supplier<LandmarkPose> targetBranch =
         () -> {
           Translation2d currPosition = swerve.getCurrentState().Pose.getTranslation();
           if (redSide.getAsBoolean()) {
-            double minDist = currPosition.getDistance(Constants.Landmarks.LEFT_LINEUP_RED[0].getTranslation());
-            int sideOfMinDist = 0;
-            for (int i = 1; i < 6; i++) {
-              if (currPosition.getDistance(Constants.Landmarks.LEFT_LINEUP_RED[i].getTranslation()) < minDist) {
-                minDist = currPosition.getDistance(Constants.Landmarks.LEFT_LINEUP_RED[i].getTranslation());
-                sideOfMinDist = i;
+            double minDist = currPosition.getDistance(RedLandmarkPose.L0.getPose().getTranslation());
+            RedLandmarkPose branchOfMinDist = RedLandmarkPose.L0;
+            for (RedLandmarkPose branch : redBranchesL) {
+              if (currPosition.getDistance(branch.getPose().getTranslation()) < minDist) {
+                minDist = currPosition.getDistance(branch.getPose().getTranslation());
+                branchOfMinDist = branch;
               }
             }
-
-            return sideOfMinDist;
+            return branchOfMinDist;
           } else {
-            double minDist = currPosition.getDistance(Constants.Landmarks.LEFT_LINEUP_BLUE[0].getTranslation());
-            int sideOfMinDist = 0;
-            for (int i = 1; i < 6; i++) {
-              if (currPosition.getDistance(Constants.Landmarks.LEFT_LINEUP_BLUE[i].getTranslation()) < minDist) {
-                minDist = currPosition.getDistance(Constants.Landmarks.LEFT_LINEUP_BLUE[i].getTranslation());
-                sideOfMinDist = i;
+            double minDist = currPosition.getDistance(BlueLandmarkPose.L0.getPose().getTranslation());
+            BlueLandmarkPose branchOfMinDist = BlueLandmarkPose.L0;
+            for (BlueLandmarkPose branch : blueBranchesL) {
+              if (currPosition.getDistance(branch.getPose().getTranslation()) < minDist) {
+                minDist = currPosition.getDistance(branch.getPose().getTranslation());
+                branchOfMinDist = branch;
               }
             }
-            return sideOfMinDist;
+            return branchOfMinDist;
           }
         };
 
-    return JamesHardenMovement.toSpecificLeftBranch(swerve, redSide, isInAuto, targetSide.get());
+    return JamesHardenMovement.toSpecificBranch(swerve, isInAuto, targetBranch.get());
   }
 
   public static JamesHardenMovement toClosestRightBranch(
       SwerveSubsystem swerve, BooleanSupplier redSide, boolean isInAuto) {
-    Supplier<Integer> targetSide =
+
+    Supplier<LandmarkPose> targetBranch =
         () -> {
           Translation2d currPosition = swerve.getCurrentState().Pose.getTranslation();
           if (redSide.getAsBoolean()) {
-            double minDist = currPosition.getDistance(Constants.Landmarks.RIGHT_LINEUP_RED[0].getTranslation());
-            int sideOfMinDist = 0;
-            for (int i = 1; i < 6; i++) {
-              if (currPosition.getDistance(Constants.Landmarks.RIGHT_LINEUP_RED[i].getTranslation()) < minDist) {
-                minDist = currPosition.getDistance(Constants.Landmarks.RIGHT_LINEUP_RED[i].getTranslation());
-                sideOfMinDist = i;
+            double minDist = currPosition.getDistance(RedLandmarkPose.R0.getPose().getTranslation());
+            RedLandmarkPose branchOfMinDist = RedLandmarkPose.R0;
+            for (RedLandmarkPose branch : redBranchesR) {
+              if (currPosition.getDistance(branch.getPose().getTranslation()) < minDist) {
+                minDist = currPosition.getDistance(branch.getPose().getTranslation());
+                branchOfMinDist = branch;
               }
             }
-
-            return sideOfMinDist;
+            return branchOfMinDist;
           } else {
-            double minDist = currPosition.getDistance(Constants.Landmarks.RIGHT_LINEUP_BLUE[0].getTranslation());
-            int sideOfMinDist = 0;
-            for (int i = 1; i < 6; i++) {
-              if (currPosition.getDistance(Constants.Landmarks.RIGHT_LINEUP_BLUE[i].getTranslation()) < minDist) {
-                minDist = currPosition.getDistance(Constants.Landmarks.RIGHT_LINEUP_BLUE[i].getTranslation());
-                sideOfMinDist = i;
+            double minDist = currPosition.getDistance(BlueLandmarkPose.R0.getPose().getTranslation());
+            BlueLandmarkPose branchOfMinDist = BlueLandmarkPose.R0;
+            for (BlueLandmarkPose branch : blueBranchesR) {
+              if (currPosition.getDistance(branch.getPose().getTranslation()) < minDist) {
+                minDist = currPosition.getDistance(branch.getPose().getTranslation());
+                branchOfMinDist = branch;
               }
             }
-            return sideOfMinDist;
+            return branchOfMinDist;
           }
         };
 
-    return JamesHardenMovement.toSpecificRightBranch(swerve, redSide, isInAuto, targetSide.get());
+    return JamesHardenMovement.toSpecificBranch(swerve, isInAuto, targetBranch.get());
   }
 }
