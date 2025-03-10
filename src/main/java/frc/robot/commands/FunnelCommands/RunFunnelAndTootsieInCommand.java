0341a1228c1a0ff3165070ed64dd5a556378a737
Rawpower9 <ritviksetty@gmail.com>
2025-03-10 16:59:18 -0700

    modified end statment to account for command getting inturrupted

--- src/main/java/frc/robot/commands/FunnelCommands/RunFunnelAndTootsieInCommand.java
+++ src/main/java/frc/robot/commands/FunnelCommands/RunFunnelAndTootsieInCommand.java
@@ -42,6 +42,6 @@ public class RunFunnelAndTootsieInCommand extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return funnelSubsystem.drakeTripped();
   }
 }
