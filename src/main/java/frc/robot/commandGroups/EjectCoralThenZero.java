7e0e07e328aae13ab995a4bd2cf5b08d8f3e6d46
Violet Su <violetnsu@gmail.com>
2025-03-10 21:06:55 -0700

    3/10 eject coral then zero

--- src/main/java/frc/robot/commandGroups/EjectCoralThenZero.java
+++ src/main/java/frc/robot/commandGroups/EjectCoralThenZero.java
@@ -1,24 +1,21 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commandGroups;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.ElevatorCommands.ZeroElevator;
 import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.subsystems.FunnelSubsystem;
 import frc.robot.subsystems.TootsieSlideSubsystem;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class EjectCoralThenZero extends SequentialCommandGroup {
-  /** Creates a new EjectCoralThenZero. */
-  public EjectCoralThenZero(ElevatorSubsystem elevatorSubsystem, TootsieSlideSubsystem tootsieSlideSubsystem) {
-    // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new EjectCoralFR(elevatorSubsystem  , tootsieSlideSubsystem));
-    addCommands(new ZeroElevator(elevatorSubsystem));
-    addCommands();
+  public EjectCoralThenZero(ElevatorSubsystem elevatorSubsystem, TootsieSlideSubsystem tootsieSlideSubsystem, FunnelSubsystem funnelSubsystem) {
+    Command a = null;
+    if (funnelSubsystem.drakeTripped()) { 
+      a = new EjectCoralFR(elevatorSubsystem, tootsieSlideSubsystem);  
+    }
+    addCommands(
+      a,                          
+      new ZeroElevator(elevatorSubsystem)  
+    );
   }
 }
