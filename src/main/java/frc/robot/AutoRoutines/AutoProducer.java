870a81eb87cd7c65cdf104a4ea726a72ffb2f7f5
Rawpower9 <ritviksetty@gmail.com>
2025-03-13 14:10:34 -0700

    got rid of the logs and seperated auto cycles into a function

--- src/main/java/frc/robot/AutoRoutines/AutoProducer.java
+++ src/main/java/frc/robot/AutoRoutines/AutoProducer.java
@@ -10,7 +10,6 @@ import frc.robot.Constants.LandmarkPose;
 import frc.robot.commandGroups.Intake;
 import frc.robot.commandGroups.JamesHardenScore;
 import frc.robot.commands.DaleCommands.ZeroArm;
-import frc.robot.commands.DebugCommands.DogLogCmd;
 import frc.robot.commands.ElevatorCommands.SetElevatorLevelInstant;
 import frc.robot.commands.ElevatorCommands.ZeroElevatorHardStop;
 import frc.robot.commands.FunnelCommands.CoralCheckedIn;
@@ -39,72 +38,109 @@ public class AutoProducer extends SequentialCommandGroup {
     // first score
     addCommands(
         new ParallelCommandGroup(
-            new ZeroArm(arm).withTimeout(1.25).alongWith(new DogLogCmd("AutoRunningWhich", 0)),
+            new ZeroArm(arm).withTimeout(1.25),
             new ParallelDeadlineGroup(
                 new SequentialCommandGroup(
-                    new ZeroElevatorHardStop(elevator)
-                        .alongWith(new DogLogCmd("AutoRunningWhich", 1)),
-                    new Intake(elevator, funnel, shooter)
-                        .alongWith(new DogLogCmd("AutoRunningWhich", 2))),
+                    new ZeroElevatorHardStop(elevator), new Intake(elevator, funnel, shooter)),
                 JamesHardenMovement.toSpecificBranch(
-                        driveTrain, true, () -> autoInformation.get(1), false)
-                    .alongWith(new DogLogCmd("AutoRunningWhich", 3)))),
+                    driveTrain, true, () -> autoInformation.get(1), false))),
         new JamesHardenScore(
-                elevator, shooter, driveTrain, ElevatorPositions.L4, true, autoInformation.get(1))
-            .alongWith(new DogLogCmd("AutoRunningWhich", 4)),
-        new SetElevatorLevelInstant(elevator, ElevatorPositions.Intake)
-            .alongWith(new DogLogCmd("AutoRunningWhich", 5)));
+            elevator, shooter, driveTrain, ElevatorPositions.L4, true, autoInformation.get(1)),
+        new SetElevatorLevelInstant(elevator, ElevatorPositions.Intake));
 
-    // first hps visit, second score
     if (autoInformation.size() > 2) {
-      addCommands(
-          new ParallelDeadlineGroup(
-              new Intake(elevator, funnel, shooter),
-              new SequentialCommandGroup(
-                  new CoralCheckedIn(funnel)
-                      .deadlineFor(
-                          new JamesHardenMovement(
-                              driveTrain,
-                              autoInformation.get(autoInformation.size() - 1).getPose(),
-                              true,
-                              false)),
-                  JamesHardenMovement.toSpecificBranch(
-                      driveTrain, true, () -> autoInformation.get(2), false))),
-          new JamesHardenScore(
-              elevator, shooter, driveTrain, ElevatorPositions.L4, true, autoInformation.get(2)),
-          new SetElevatorLevelInstant(elevator, ElevatorPositions.Intake));
+      autoCycle(
+          elevator,
+          funnel,
+          shooter,
+          driveTrain,
+          autoInformation.get(2),
+          autoInformation.get(autoInformation.size() - 1));
     }
-
     if (autoInformation.size() > 3) {
-      addCommands(
-          new ParallelDeadlineGroup(
-              new Intake(elevator, funnel, shooter),
-              new SequentialCommandGroup(
-                  new CoralCheckedIn(funnel)
-                      .deadlineFor(
-                          new JamesHardenMovement(
-                              driveTrain,
-                              autoInformation.get(autoInformation.size() - 1).getPose(),
-                              true,
-                              false)),
-                  JamesHardenMovement.toSpecificBranch(
-                      driveTrain, true, () -> autoInformation.get(3), false))),
-          new JamesHardenScore(
-              elevator, shooter, driveTrain, ElevatorPositions.L4, true, autoInformation.get(3)),
-          new SetElevatorLevelInstant(elevator, ElevatorPositions.Intake));
+      autoCycle(
+          elevator,
+          funnel,
+          shooter,
+          driveTrain,
+          autoInformation.get(3),
+          autoInformation.get(autoInformation.size() - 1));
     }
+    // first hps visit, second score
+    // if (autoInformation.size() > 2) {
+    //   addCommands(
+    //       new ParallelDeadlineGroup(
+    //           new Intake(elevator, funnel, shooter),
+    //           new SequentialCommandGroup(
+    //               new CoralCheckedIn(funnel)
+    //                   .deadlineFor(
+    //                       new JamesHardenMovement(
+    //                           driveTrain,
+    //                           autoInformation.get(autoInformation.size() - 1).getPose(),
+    //                           true,
+    //                           false)),
+    //               JamesHardenMovement.toSpecificBranch(
+    //                   driveTrain, true, () -> autoInformation.get(2), false))),
+    //       new JamesHardenScore(
+    //           elevator, shooter, driveTrain, ElevatorPositions.L4, true, autoInformation.get(2)),
+    //       new SetElevatorLevelInstant(elevator, ElevatorPositions.Intake));
+    // }
 
+    // if (autoInformation.size() > 3) {
+    //   addCommands(
+    //       new ParallelDeadlineGroup(
+    //           new Intake(elevator, funnel, shooter),
+    //           new SequentialCommandGroup(
+    //               new CoralCheckedIn(funnel)
+    //                   .deadlineFor(
+    //                       new JamesHardenMovement(
+    //                           driveTrain,
+    //                           autoInformation.get(autoInformation.size() - 1).getPose(),
+    //                           true,
+    //                           false)),
+    //               JamesHardenMovement.toSpecificBranch(
+    //                   driveTrain, true, () -> autoInformation.get(3), false))),
+    //       new JamesHardenScore(
+    //           elevator, shooter, driveTrain, ElevatorPositions.L4, true, autoInformation.get(3)),
+    //       new SetElevatorLevelInstant(elevator, ElevatorPositions.Intake));
+    // }
+
+    // addCommands(
+    //     new ParallelCommandGroup(
+    //         new Intake(elevator, funnel, shooter),
+    //         new SequentialCommandGroup(
+    //             new CoralCheckedIn(funnel)
+    //                 .deadlineFor(
+    //                     new JamesHardenMovement(
+    //                         driveTrain,
+    //                         autoInformation.get(autoInformation.size() - 1).getPose(),
+    //                         true,
+    //                         false))
+    //                 .andThen(
+    //                     new JamesHardenScore(
+    //                         elevator,
+    //                         shooter,
+    //                         driveTrain,
+    //                         ElevatorPositions.L4,
+    //                         true,
+    //                         autoInformation.get(3))),
+    //             new SetElevatorLevelInstant(elevator, ElevatorPositions.Intake))));
+  }
+
+  private void autoCycle(
+      ElevatorSubsystem elevator,
+      FunnelSubsystem funnel,
+      TootsieSlideSubsystem shooter,
+      SwerveSubsystem driveTrain,
+      LandmarkPose scorePosition,
+      LandmarkPose HPSPosition) {
     addCommands(
         new ParallelCommandGroup(
             new Intake(elevator, funnel, shooter),
             new SequentialCommandGroup(
                 new CoralCheckedIn(funnel)
                     .deadlineFor(
-                        new JamesHardenMovement(
-                            driveTrain,
-                            autoInformation.get(autoInformation.size() - 1).getPose(),
-                            true,
-                            false))
+                        new JamesHardenMovement(driveTrain, HPSPosition.getPose(), true, false))
                     .andThen(
                         new JamesHardenScore(
                             elevator,
@@ -112,7 +148,7 @@ public class AutoProducer extends SequentialCommandGroup {
                             driveTrain,
                             ElevatorPositions.L4,
                             true,
-                            autoInformation.get(3))),
+                            scorePosition)),
                 new SetElevatorLevelInstant(elevator, ElevatorPositions.Intake))));
   }
 }
