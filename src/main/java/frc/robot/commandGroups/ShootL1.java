abd721f65552758c6fbdacad88eca544723b53b1
Sid Senthil <sidsenthil2010@gmail.com>
2025-10-08 19:13:56 -0700

    oops forgot to commit some

--- src/main/java/frc/robot/commandGroups/ShootL1.java
+++ src/main/java/frc/robot/commandGroups/ShootL1.java
@@ -8,7 +8,8 @@ import frc.robot.subsystems.ElevatorSubsystemMD2;
 import frc.robot.subsystems.TootsieSlideSubsystem;
 
 public class ShootL1 extends SequentialCommandGroup {
-  public ShootL1(ElevatorSubsystemMD2 elevatorSubsystem, TootsieSlideSubsystem tootsieSlideSubsystem) {
+  public ShootL1(
+      ElevatorSubsystemMD2 elevatorSubsystem, TootsieSlideSubsystem tootsieSlideSubsystem) {
     addCommands(
         new SetElevatorLevel(elevatorSubsystem, ElevatorPositions.L1, false)
             .andThen(new ShootSlow(tootsieSlideSubsystem))
