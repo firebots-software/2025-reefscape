a6e9cf086ebe69e282ae0c1004e1dc73200e21c3
Parneel Bhakhri <pbhakhrism@gmail.com>
2025-10-08 19:18:52 -0700

    remove hardcoded values

--- src/main/java/frc/robot/subsystems/ElevatorSubsystemMD2.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystemMD2.java
@@ -5,12 +5,18 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.Slot1Configs;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.TorqueCurrentFOC;
 import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
+
 import dev.doglog.DogLog;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -45,20 +51,57 @@ public class ElevatorSubsystemMD2 extends SubsystemBase {
     Follower follower = new Follower(1, false);
     motor2.setControl(follower);
 
-    Slot0Configs s0c = new Slot0Configs().withKP(1.0).withKI(1.0).withKD(1.0);
-
     motor1.updateCurrentLimits(1.0, 1.0);
     motor2.updateCurrentLimits(1.0, 1.0);
 
-    MotionMagicConfigs mmc =
-        new MotionMagicConfigs()
-            .withMotionMagicAcceleration(ElevatorConstants.ACCELERATION)
-            .withMotionMagicCruiseVelocity(ElevatorConstants.CRUISE_VELOCITY);
+    MotionMagicConfigs mmc = new MotionMagicConfigs()
+        .withMotionMagicAcceleration(ElevatorConstants.ACCELERATION)
+        .withMotionMagicCruiseVelocity(ElevatorConstants.CRUISE_VELOCITY);
 
     TalonFXConfigurator m1Config = motor1.getConfigurator();
+    TalonFXConfigurator m2Config = motor2.getConfigurator();
+
+    Slot1Configs s1c = new Slot1Configs()
+        .withKP(ElevatorConstants.S1C_KP)
+        .withKI(ElevatorConstants.S1C_KI)
+        .withKD(ElevatorConstants.S1C_KD)
+        .withKS(ElevatorConstants.S0C_KS)
+        .withKG(ElevatorConstants.S0C_KG)
+        .withKA(ElevatorConstants.S0C_KA)
+        .withKV(ElevatorConstants.S0C_KV)
+        .withGravityType(GravityTypeValue.Elevator_Static)
+        .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
+
+    Slot0Configs s0c = new Slot0Configs()
+        .withKP(ElevatorConstants.S0C_KP)
+        .withKI(ElevatorConstants.S0C_KI)
+        .withKD(ElevatorConstants.S0C_KD)
+        .withKS(ElevatorConstants.S0C_KS)
+        .withKG(ElevatorConstants.S0C_KG)
+        .withKA(ElevatorConstants.S0C_KA)
+        .withKV(ElevatorConstants.S0C_KV)
+        .withGravityType(GravityTypeValue.Elevator_Static)
+        .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
 
     m1Config.apply(s0c);
+    m2Config.apply(s0c);
+    m1Config.apply(s1c);
+    m2Config.apply(s1c);
+
+    MotorOutputConfigs moc = new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake);
+
+    mmc = new MotionMagicConfigs();
+    mmc.MotionMagicCruiseVelocity = ElevatorConstants.MOTIONMAGIC_MAX_VELOCITY;
+    mmc.MotionMagicAcceleration = ElevatorConstants.MOTIONMAGIC_MAX_ACCELERATION;
+
     m1Config.apply(mmc);
+    m2Config.apply(mmc);
+
+    m1Config.apply(moc);
+    m2Config.apply(moc);
+
+    master = motor1;
+    resetPositionFiltered();
   }
 
   public void setHeight(double height) {
