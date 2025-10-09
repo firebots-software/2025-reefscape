7b0accc66bce8e02cd45e34392dbba7f03734054
Saketh <sgiri331@student.fuhsd.org>
2025-10-08 18:48:18 -0700

    small edits to ElevatorSubsystem2

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem2.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem2.java
@@ -1,4 +1,3 @@
-
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
@@ -6,130 +5,172 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
-import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.Slot1Configs;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.GravityTypeValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
+import com.ctre.phoenix6.controls.TorqueCurrentFOC;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.CANrange;
 import dev.doglog.DogLog;
+import edu.wpi.first.math.filter.LinearFilter;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.ElevatorConstants;
-import frc.robot.util.LoggedTalonFX;;
-// import frc.robot.Constants.Elevator.ElevatorPositions;
+import frc.robot.Constants.ElevatorConstants.ElevatorPositions;
+import frc.robot.util.LoggedTalonFX;
 
-public class ElevatorSubsystem2 extends SubsystemBase {
-    private TalonFX motor1;
-    private TalonFX motor2;
-    public TalonFX master;
-    public double targetHeight;
+public class ElevatorSubsystem extends SubsystemBase {
+  private static ElevatorSubsystem instance;
 
-    private MotionMagicConfigs mmc;
+  private LoggedTalonFX motor1;
+  private LoggedTalonFX motor2;
+  public LoggedTalonFX master;
+  private CANrange distance;
+  private LinearFilter elevatorFilter;
 
-    private final MotionMagicVoltage controlRequest = new MotionMagicVoltage(0);
+  private final MotionMagicVoltage controlRequest = new MotionMagicVoltage(null);
+  private final TorqueCurrentFOC torqueRequest = new TorqueCurrentFOC(null);
+  private final VelocityVoltage velocityRequest = new VelocityVoltage(null);
 
-    public ElevatorSubsystem2() {
-        motor1 =
-        new TalonFX(
-            ElevatorConstants.MOTOR1_PORT,
-            Constants.Swerve.WHICH_SWERVE_ROBOT.CANBUS_NAME);
-        motor2 =
-        new LoggedTalonFX(
-            "subsystems/Elevator/motor2",
-            ElevatorConstants.MOTOR2_PORT,
-            Constants.Swerve.WHICH_SWERVE_ROBOT.CANBUS_NAME);
+  private ElevatorPositions currLevel;
+  private double tolerance = 3.0;
+  private boolean elevatorZeroed;
+  private double currentHeightToF;
 
-        Follower follower = new Follower(ElevatorConstants.MOTOR1_PORT, false);
-        motor2.setControl(follower);
+  public ElevatorSubsystem() {
+    motor1 = new LoggedTalonFX(ElevatorConstants.MOTOR1_PORT);
+    motor2 = new LoggedTalonFX(ElevatorConstants.MOTOR2_PORT);
+    distance =
+        new CANrange(
+            ElevatorConstants.CANRANGE_PORT, Constants.Swerve.WHICH_SWERVE_ROBOT.CANBUS_NAME);
+    elevatorZeroed = false;
+    elevatorFilter = LinearFilter.singlePoleIIR(0.1, 0.02);
 
+    master = motor1;
+    Follower follower = new Follower(ElevatorConstants.MOTOR1_PORT, false);
+    motor2.setControl(follower);
 
-        Slot1Configs s1c =
+    TalonFXConfigurator m1Config = motor1.getConfigurator();
+
+    Slot0Configs s0c =
+        new Slot0Configs()
+            .withKP(ElevatorConstants.S0C_KP)
+            .withKI(ElevatorConstants.S0C_KI)
+            .withKD(ElevatorConstants.S0C_KD);
+
+    Slot1Configs s1c =
         new Slot1Configs()
-            // .withKP(ElevatorConstants.S1C_KP)
-            // .withKI(ElevatorConstants.S1C_KI)
-            // .withKD(ElevatorConstants.S1C_KD)
+            .withKP(ElevatorConstants.S1C_KP)
+            .withKI(ElevatorConstants.S1C_KI)
+            .withKD(ElevatorConstants.S1C_KD)
             .withKS(ElevatorConstants.S0C_KS)
             .withKG(ElevatorConstants.S0C_KG)
             .withKA(ElevatorConstants.S0C_KA)
-            .withKV(ElevatorConstants.S0C_KV)
-            .withGravityType(GravityTypeValue.Elevator_Static)
-            .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
+            .withKV(ElevatorConstants.S0C_KV);
 
-        Slot0Configs s0c =
-            new Slot0Configs()
-                .withKP(ElevatorConstants.S0C_KP)
-                .withKI(ElevatorConstants.S0C_KI)
-                .withKD(ElevatorConstants.S0C_KD)
-                .withKS(ElevatorConstants.S0C_KS)
-                .withKG(ElevatorConstants.S0C_KG)
-                .withKA(ElevatorConstants.S0C_KA)
-                .withKV(ElevatorConstants.S0C_KV)
-                .withGravityType(GravityTypeValue.Elevator_Static)
-                .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
+    motor1.updateCurrentLimits(
+        ElevatorConstants.STATOR_CURRENT_LIMIT, ElevatorConstants.SUPPLY_CURRENT_LIMIT);
 
-        ((LoggedTalonFX) motor1).updateCurrentLimits(
-            ElevatorConstants.STATOR_CURRENT_LIMIT_AMPS, ElevatorConstants.SUPPLY_CURRENT_LIMIT_AMPS);
-        ((LoggedTalonFX) motor2).updateCurrentLimits(
-            ElevatorConstants.STATOR_CURRENT_LIMIT, ElevatorConstants.SUPPLY_CURRENT_LIMIT);
+    MotionMagicConfigs mmc =
+        new MotionMagicConfigs()
+            .withMotionMagicAcceleration(ElevatorConstants.ACCELERATION)
+            .withMotionMagicCruiseVelocity(ElevatorConstants.CRUISE_VELOCITY);
 
-        TalonFXConfigurator m1Config = motor1.getConfigurator();
-        TalonFXConfigurator m2Config = motor2.getConfigurator();
+    m1Config.apply(s0c);
+    m1Config.apply(mmc);
 
-        m1Config.apply(s0c);
-        m2Config.apply(s0c);
-        m1Config.apply(s1c);
-        m2Config.apply(s1c);
+    currentHeightToF = elevatorFilter.calculate(getToFDistance());
+  }
 
-        MotorOutputConfigs moc = new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake);
-
-        // Apply MotionMagic to motors
-        mmc = new MotionMagicConfigs();
-        mmc.MotionMagicCruiseVelocity = ElevatorConstants.MOTIONMAGIC_MAX_VELOCITY;
-        mmc.MotionMagicAcceleration = ElevatorConstants.MOTIONMAGIC_MAX_ACCELERATION;
-
-        m1Config.apply(mmc);
-        m2Config.apply(mmc);
-
-        m1Config.apply(moc);
-        m2Config.apply(moc);
-
-        master = motor1;
+  public static ElevatorSubsystem getInstance() {
+    if (instance == null) {
+      instance = new ElevatorSubsystem();
     }
+    return instance;
+  }
 
-    public void setPosition(double height) {
-        if (height < Constants.ElevatorConstants.minHeight) height = Constants.ElevatorConstants.minHeight;
-        if (height > Constants.ElevatorConstants.maxHeight) height = Constants.ElevatorConstants.maxHeight;
-        
-        master.setControl(
-            controlRequest  
-                .withPosition(
-                    height
-                        * ElevatorConstants.CONVERSION_FACTOR_UP_DISTANCE_TO_ROTATIONS
-                        / ElevatorConstants.CARRAIGE_UPDUCTION)
-                .withSlot(0));
-        DogLog.log(
-            "subsystems/Elevator/elevatorSetpoint(rot)",
-            height
-                * ElevatorConstants.CONVERSION_FACTOR_UP_DISTANCE_TO_ROTATIONS
-                / ElevatorConstants.CARRAIGE_UPDUCTION);
-    }
+  public void setHeight(ElevatorPositions level) {
+    currLevel = level;
+    master.setControl(controlRequest.withPosition(level.height));
+  }
 
-    public void resetElevatorPositionToZero() {
-        master.setPosition(0);
-    }
+  public ElevatorPositions getLevel() {
+    return currLevel;
+  }
 
-    public double getHeight() {
-        return master.getRotorPosition().getValueAsDouble()* ElevatorConstants.CONVERSION_FACTOR_UP_DISTANCE_TO_ROTATIONS/ ElevatorConstants.CARRAIGE_UPDUCTION;
-    }
+  public double getErrorDist() {
+    return ((currLevel.height * ElevatorConstants.CONVERSION_FACTOR_UP_DISTANCE_TO_ROTATIONS)
+            / ElevatorConstants.CARRAIGE_UPDUCTION)
+        - master.getPosition().getValueAsDouble();
+  }
 
-    public void periodic() {
-        DogLog.log(
-            "subsystems/Elevator/currentheight",
-            getHeight());
+  public boolean isAtPosition() {
+    return (Math.abs(getErrorDist())) <= tolerance;
+  }
+
+  public boolean atIntake() {
+    return currLevel.equals(ElevatorPositions.Intake);
+  }
+
+  public void zeroElevator() {
+    master.setPosition(0);
+  }
+
+  public void elevatorHasBeenZeroed() {
+    elevatorZeroed = true;
+  }
+
+  public boolean isElevatorZeroed() {
+    return elevatorZeroed;
+  }
+
+  public void ElevatorTorqueMode() {
+    master.setControl(torqueRequest.withOutput(ElevatorConstants.ELEVATOR_TORQUE));
+  }
+
+  public void reduceCurrentLimits() {
+    master.updateCurrentLimits(30, 10);
+  }
+
+  public void resetCurrentLimits() {
+    master.updateCurrentLimits(
+        ElevatorConstants.STATOR_CURRENT_LIMIT, ElevatorConstants.SUPPLY_CURRENT_LIMIT);
+  }
+
+  public void moveElevatorNegative() {
+    master.setControl(velocityRequest.withVelocity(-5).withSlot(1));
+  }
+
+  public boolean checkCurrent() {
+    double Supplycurrent = Math.abs(master.getSupplyCurrent().getValue().magnitude());
+    double Statorcurrent = Math.abs(master.getStatorCurrent().getValue().magnitude());
+    if (Supplycurrent > 1.0 && Statorcurrent > 20) {
+      return true;
     }
+    return false;
+  }
+
+  public void resetPositionFiltered() {
+    master.setPosition(
+        currentHeightToF * ElevatorConstants.CONVERSION_FACTOR_UP_DISTANCE_TO_ROTATIONS);
+  }
+
+  public double getToFDistance() {
+    // 0.11 is the sensor offset
+    return distance.getDistance().getValueAsDouble() - Constants.ElevatorConstants.SENSOR_OFFSET;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    DogLog.log("Current angle", getLevel());
+    DogLog.log("Is at target", isAtPosition());
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
 }
